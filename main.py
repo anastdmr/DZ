f603b6f72489958fefdae0df795e0a1ac34ae595
anastdmr <nastyakokovina26@gmail.com>
2024-10-16 15:29:28 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1st program
+print (9 ** 0.5 * 5)
+# 2nd program
+print (9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print ( (2 * 2 + 2) == (2 *( 2 + 2)))
+# 4th program
+s = '123.456'
+n = float(s)*10
+print (int(n%10))
